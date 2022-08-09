8301d54029e6d37c672af617040ee2cc6e8e0c05
FhLk <paramat10042545@hotmail.com>
2022-08-09 15:29:00 +0700

    Move Project

---
+++ src/test/java/oasip/OasipSsa5ApplicationTests.java
@@ -0,0 +1,13 @@
+package oasip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OasipSsa5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
