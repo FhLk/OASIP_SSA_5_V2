a3efded5f2ef492253a6574d9cd0986c1f6641b5
taohoo3698 <ampla14@gmail.com>
2022-08-18 17:37:25 +0700

    getuser

--- Back-end/src/main/java/oasip/Repository/CategoryRepository.java
+++ Back-end/src/main/java/oasip/Repository/CategoryRepository.java
@@ -3,5 +3,5 @@ package oasip.Repository;
 import oasip.Entity.EventCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EventCategoryRepository extends JpaRepository<EventCategory,Integer> {
+public interface CategoryRepository extends JpaRepository<EventCategory,Integer> {
 }
