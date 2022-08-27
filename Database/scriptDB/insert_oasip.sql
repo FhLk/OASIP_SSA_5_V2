b9e28d4970c5a223ecee1d11f683dc4b80f65bf1
Name63130500043 <noteatomic678@gmail.com>
2022-08-27 22:31:45 +0700

    edit db

--- Database/scriptDB/insert_oasip.sql
+++ Database/scriptDB/insert_oasip.sql
@@ -8,15 +8,15 @@ insert into `eventcategory` (`category_id`,`category_name`,`description`,`durati
 select * from user;
 delete from user;
 insert into user values 
-	("OASIP ADMIN","oasip.admin@kmutt.ac.th",1,'2022-08-01 00:00:00','2022-08-01 00:00:00','00000000'),
-    ("Somchai Jaidee","somchai.jai@kmutt.ac.th",2,'2022-08-08 15:00:00','2022-08-08 15:00:00','111111111'),
-    ("Komkrid Rakdee","komkrid.rak@mail.kmutt.ac.th",3,'2022-08-08 15:00:01','2022-08-08 15:00:01','2222222222'),
-    ("สมเกียรติ ขยันเรียน","somkiat.kay@kmutt.ac.th",3,'2022-08-16 09:00:00','2022-08-16 09:00:00','33333333333');
+	("1","OASIP ADMIN","oasip.admin@kmutt.ac.th",1,'2022-08-01 00:00:00','2022-08-01 00:00:00','00000000'),
+    ("2","Somchai Jaidee","somchai.jai@kmutt.ac.th",2,'2022-08-08 15:00:00','2022-08-08 15:00:00','111111111'),
+    ("3","Komkrid Rakdee","komkrid.rak@mail.kmutt.ac.th",3,'2022-08-08 15:00:01','2022-08-08 15:00:01','2222222222'),
+    ("4","สมเกียรติ ขยันเรียน","somkiat.kay@kmutt.ac.th",3,'2022-08-16 09:00:00','2022-08-16 09:00:00','33333333333');
     
-insert into `eventbooking` (`booking_id`,`booking_name`,`booking_email`,`start_time`,`booking_duration`,`event_note`,`category`,`user_name`) values 
-(1,'Somchai Jaidee (OR-7)','somchai.jai@mail.kmutt.ac.th','2022-05-23 13:00',30,' ',2,"Somchai Jaidee"),
-(2,'Somsri Rakdee (SJ-3)','somsri.rak@mail.kmutt.ac.th','2022-04-27 09:30',30,' ขอปรึกษาเรื่องเพื่อนไม่ช่วยงาน ',1,"Komkrid Rakdee"),
-(3,'สมเกียรติ ขยันเรียน กลุ่ม TT-4','somkiat.kay@kmutt.ac.th','2022-05-23 16:30',15,' ',3,"สมเกียรติ ขยันเรียน");
+insert into `eventbooking` (`booking_id`,`booking_name`,`booking_email`,`start_time`,`booking_duration`,`event_note`,`category`,`user_id_student`) values 
+(1,'Somchai Jaidee (OR-7)','somchai.jai@mail.kmutt.ac.th','2022-05-23 13:00',30,' ',2,"Somchai Jaidee","2"),
+(2,'Somsri Rakdee (SJ-3)','somsri.rak@mail.kmutt.ac.th','2022-04-27 09:30',30,' ขอปรึกษาเรื่องเพื่อนไม่ช่วยงาน ',1,"Komkrid Rakdee","5"),
+(3,'สมเกียรติ ขยันเรียน กลุ่ม TT-4','somkiat.kay@kmutt.ac.th','2022-05-23 16:30',15,' ',3,"สมเกียรติ ขยันเรียน","4");
 
-
+insert into `eventcategory_owner` (`eventcategory_category_id`,`user_id_lecturer`) values (2,2),(3,2);
 
