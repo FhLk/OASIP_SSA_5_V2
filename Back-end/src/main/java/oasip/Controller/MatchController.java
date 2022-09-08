67688adb8a5de71ccf45bf6fd0f5a69e50bec1d7
FhLk <68800909+FhLk@users.noreply.github.com>
2022-09-08 12:03:30 +0700

    Update MatchController.java

--- Back-end/src/main/java/oasip/Controller/MatchController.java
+++ Back-end/src/main/java/oasip/Controller/MatchController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/login")
+@RequestMapping("/api/match")
 public class MatchController {
     @Autowired private MatchService service;
     @Autowired private ModelMapper modelMapper;
