b7de177d8bf8b6e04bec2754e94d460db5daddbf
FhLk <68800909+FhLk@users.noreply.github.com>
2022-09-28 00:11:03 +0700

    Update CheckAuthenController.java

--- Back-end/src/main/java/oasip/Controller/CheckAuthenController.java
+++ Back-end/src/main/java/oasip/Controller/CheckAuthenController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/checkAuthen")
-@CrossOrigin(origins = "http://localhost:3000/",allowedHeaders = "*",allowCredentials = "true",
-        methods = {RequestMethod.DELETE,RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT})
 public class CheckAuthenController {
     @Autowired private MatchService service;
     @Autowired private ModelMapper modelMapper;
