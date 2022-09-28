773d4cfa289f2f289f4504d5edb951ddefc04105
FhLk <68800909+FhLk@users.noreply.github.com>
2022-09-28 21:44:36 +0700

    Update UserService.java

--- Back-end/src/main/java/oasip/Service/UserService.java
+++ Back-end/src/main/java/oasip/Service/UserService.java
@@ -68,7 +68,7 @@ public class UserService  {
         List<EventUser> duplicateName = repository.findByName(user.getName());
         EventUser duplicateEmail = repository.findByEmail(user.getEmail());
         List<String> errors = new ArrayList<>();
-        if (!duplicateName.isEmpty() && duplicateEmail == null) {
+        if (!duplicateName.isEmpty() && duplicateEmail != null) {
             errors.add("This Username and Email are already use!!!");
             throw new UserException(errors.toString());
         }
@@ -77,7 +77,7 @@ public class UserService  {
 
             throw new UserException(errors.toString());
         }
-        if (duplicateEmail == null) {
+        if (duplicateEmail != null) {
             errors.add("This Email is already use!!!");
             throw new UserException(errors.toString());
         }
