fb5a2816f95b6bb4107e6b09f5e2c4064eae2e37
FhLk <68800909+FhLk@users.noreply.github.com>
2022-10-04 02:10:42 +0700

    Update UserStatus.vue

--- Front-end/OASIP-Client/src/components/UserStatus.vue
+++ Front-end/OASIP-Client/src/components/UserStatus.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { onBeforeMount, ref } from 'vue';
-const pic = ref("src/assets/user.png")
+// const pic = ref()
 const name = ref("")
 const role = ref("")
 onBeforeMount(() => {
@@ -11,7 +11,7 @@ onBeforeMount(() => {
  
 <template>
     <div class="bgUser hover:bg-[#14D3FF] flex p-1.5 rounded-lg">
-        <img :src="pic" class="imgid shadow-gray-600 shadow-md">
+        <img src="../assets/user.png" class="imgid shadow-gray-600 shadow-md">
         <div class="ml-1 text-center">
             <p>{{name}}</p>
             <p class="">{{role}}</p>
