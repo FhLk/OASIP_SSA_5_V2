13a14b68ad7b2f0c92c46fcfca37830082778a28
taohoo3698 <ampla14@gmail.com>
2022-10-09 22:15:37 +0700

    add endpoint customauthoriz

--- Back-end/src/main/java/oasip/security/SecurityConfig.java
+++ Back-end/src/main/java/oasip/security/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         customAuthenticationFilter.setFilterProcessesUrl("/api/login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().antMatchers("/api/login/**","/api/token/refresh","/api/checkAuthen").permitAll();
+//        http.authorizeRequests().antMatchers("/api/login/**","/api/token/refresh","/api/checkAuthen").permitAll();
 //        http.authorizeRequests().antMatchers("/api/**").permitAll();
         http.authorizeRequests().antMatchers(POST,"/api/bookings").permitAll();
         http.authorizeRequests().antMatchers("/api/login/**","/api/token/refresh","/api/checkAuthen","/api/categories/**").permitAll();
